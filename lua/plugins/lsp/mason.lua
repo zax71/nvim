7ba5ddf4cd2392047f513b2b7db5b87bd92a857d
Zax71 <zabax32@gmail.com>
2024-07-02 17:55:16 +0100

    Setup Formatters

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,39 +1,54 @@
 return {
-    "williamboman/mason.nvim",
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-    },
-    config = function()
-        -- import mason
-        local mason = require("mason")
-        
-        -- load mason-lspconfig
-        local mason_lspconfig = require("mason-lspconfig") 
-        -- enable mason and configure icons
-        mason.setup({
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗",
-                },
-            },
-        })
+    "williamboman/mason-lspconfig.nvim",
+	dependencies = {
+        "williamboman/mason.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
 
-        mason_lspconfig.setup({
-            -- list of servers for mason to install
-            ensure_installed = {
-                "typos_lsp", -- Generic spell check
-                "neocmake", -- Makefile
-                "tsserver", -- .ts
-                "html", -- .html
-                "cssls", -- .css
-                "tailwindcss", -- .css
-                "svelte",
-                "lua_ls",
-                "emmet_ls",
-                "pyright",
-            },
-        })
-    end,
+		-- load mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
+
+		-- Load mason tool installer
+		local mason_tool_installer = require("mason-tool-installer")
+
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"typos_lsp", -- Generic spell check
+				"neocmake", -- Makefile
+				"tsserver", -- .ts
+				"html", -- .html
+				"cssls", -- .css
+				"tailwindcss", -- .css
+				"svelte", -- .svelte
+				"lua_ls", -- .lua
+				"emmet_ls", -- HTML esque
+				"pyright", -- .py
+			},
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier", -- js
+				"stylua", -- lua
+				"isort", -- python
+				"black", -- python
+			},
+			auto_update = true,
+		})
+	end,
 }

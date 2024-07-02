66e7969e092edfe7f5c7a57ed9d8ed0fb7179296
Zax71 <zabax32@gmail.com>
2024-07-02 19:19:17 +0100

    move linting and formatting in to lsp/

---
+++ lua/plugins/lsp/formatting.lua
@@ -0,0 +1,34 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { { "prettierd", "prettier" } },
+				typescript = { { "prettierd", "prettier" } },
+				javascriptreact = { { "prettierd", "prettier" } },
+				typescriptreact = { { "prettierd", "prettier" } },
+				svelte = { { "prettierd", "prettier" } },
+				css = { { "prettierd", "prettier" } },
+				html = { { "prettierd", "prettier" } },
+				json = { { "prettierd", "prettier" } },
+				yaml = { { "prettierd", "prettier" } },
+				markdown = { { "prettierd", "prettier" } },
+				graphql = { { "prettierd", "prettier" } },
+				liquid = { { "prettierd", "prettier" } },
+
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				rust = { "rustfmt" },
+				go = { "gofumpt" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout = 500,
+			},
+		})
+	end,
+}

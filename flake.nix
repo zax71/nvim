5be5c80834011ce13062a974b6dbc75512215b23
Zax71 <67716263+zax71@users.noreply.github.com>
2024-11-25 15:17:59 +0000

    Setup LSPs and formatters

--- flake.nix
+++ flake.nix
@@ -1,37 +1,40 @@
 # Based off https://primamateria.github.io/blog/neovim-nix/
 {
-    description = "Zax71's Neovim flake";
+  description = "Zax71's Neovim flake";
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-        neovim = {
-            url = "github:nix-community/neovim-nightly-overlay";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    neovim = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
 
-    outputs = { self, nixpkgs, neovim }:
-        let
-            overlayFlakeInputs = prev: final: {
-                neovim = neovim.packages.x86_64-linux.neovim;
-            };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      neovim,
+    }:
+    let
+      overlayFlakeInputs = prev: final: { neovim = neovim.packages.x86_64-linux.neovim; };
 
-            overlayMyNeovim = prev: final: {
-                zax-neovim = import ./packages/zax-neovim.nix {
-                    pkgs = final;
-                };
-            };
+      overlayMyNeovim = prev: final: { zax-neovim = import ./packages/zax-neovim.nix { pkgs = final; }; };
 
-            pkgs = import nixpkgs {
-                system = "x86_64-linux";
-                overlays = [ overlayFlakeInputs overlayMyNeovim ];
-            };
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [
+          overlayFlakeInputs
+          overlayMyNeovim
+        ];
+      };
 
-        in {
-            packages.x86_64-linux.default = pkgs.myNeovim;
-            apps.x86_64-linux.default = {
-                type = "app";
-                program = "${pkgs.zax-neovim}/bin/nvim";
-            };
-        };
+    in
+    {
+      packages.x86_64-linux.default = pkgs.myNeovim;
+      apps.x86_64-linux.default = {
+        type = "app";
+        program = "${pkgs.zax-neovim}/bin/nvim";
+      };
+    };
 }
--- runtimeDeps.nix
+++ runtimeDeps.nix
@@ -1,40 +1,42 @@
 { pkgs }:
-with pkgs; [
-#   -- Plugin deps --
-    
-    fd
+with pkgs;
+[
+  #   -- Plugin deps --
 
-#      -- LSP --
+  fd
 
-    typos-lsp # Spell check
-    neocmakelsp # Cmake
-    luajitPackages.lua-lsp # Lua
-    mypy # Python
-    rust-analyzer # Rust
-    gopls # Go
-    slint-lsp
+  #      -- LSP --
 
-#   -- Formatters --
+  typos-lsp # Spell check
+  neocmakelsp # Cmake
+  lua-language-server # Lua
+  pyright # Python
+  rust-analyzer # Rust
+  gopls # Go
+  slint-lsp # Slint
+  nixd # Nix
 
-    stylua # Lua
-    isort # Python
-    black # Python
-    gofumpt # Go
-    taplo # TOML
-    rustfmt # Rust
+  #   -- Formatters --
 
-#    -- Linters --
-    pylint
-    luajitPackages.luacheck
-    golangci-lint
-    clippy # Rust
+  stylua # Lua
+  isort # Python
+  black # Python
+  gofumpt # Go
+  taplo # TOML
+  rustfmt # Rust
+  nixfmt-rfc-style # Nix
 
+  #    -- Linters --
+  pylint
+  luajitPackages.luacheck
+  golangci-lint
+  clippy # Rust
 
-    # packages with results in /lib/node_modules/.bin must come at the end
-    pyright
-    emmet-ls
-    nodePackages.typescript
-    nodePackages.typescript-language-server
-    nodePackages.eslint_d
-    nodePackages.prettier
+  # packages with results in /lib/node_modules/.bin must come at the end
+  pyright
+  emmet-ls
+  nodePackages.typescript
+  nodePackages.typescript-language-server
+  nodePackages.eslint_d
+  nodePackages.prettier
 ]

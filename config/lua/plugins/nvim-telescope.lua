457f6703b69eb7d117ce25d75cce6db15cabfaa0
Zax71 <67716263+zax71@users.noreply.github.com>
2024-11-25 14:50:37 +0000

    Hide gitignored files in Telescope

--- config/lua/plugins/nvim-telescope.lua
+++ config/lua/plugins/nvim-telescope.lua
@@ -1,20 +1,29 @@
 local telescope = require("telescope")
 local actions = require("telescope.actions")
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
 
 telescope.setup({
-    defaults = {
-            mappings = {
-                i = {
-                    ["<C-k>"] = actions.move_selection_previous,
-                    ["<C-j>"] = actions.move_selection_next,
-            }
-        },
-    }
+	defaults = {
+		ripgrep_arguments = {
+			"rg",
+			"--hidden",
+			"--no-heading",
+			"--with-filename",
+			"--line-number",
+			"--column",
+			"--smart-case",
+		},
+		mappings = {
+			i = {
+				["<C-k>"] = actions.move_selection_previous,
+				["<C-j>"] = actions.move_selection_next,
+			},
+		},
+	},
 })
 
 telescope.load_extension("fzf")
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
